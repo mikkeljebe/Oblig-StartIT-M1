20a315638ad8800229f658540628d0470c53a311
mikkeljebe <86236162+mikkeljebe@users.noreply.github.com>
2021-09-20 11:06:19 +0200

    controllerfunksjoner i egen fil

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
  let numbers = [7, 3, 1, 5, 8];
  let chosenBar; // Variabel for hvilken stolpe som er valgt
  let inputValue; // Variabel for hva som er skrevet i input-feltet
- let enableDisableButtons = 'disabled'
+ let enableDisableButtons = 'disabled';
  
   // view
   show();
@@ -51,32 +51,3 @@
   }
 
 //controller
-function velgStolpe(x){
-  chosenBar == x ? chosenBar = undefined : chosenBar = x;
-  chosenBar == undefined 
-  ? enableDisableButtons = 'disabled' 
-  : enableDisableButtons = '';
-  show()
-}
-
-function endreStolpe() {
-  inputValue > 10 || inputValue < 1 || inputValue == undefined 
-  ? alert('Du må velge en verdi mellom 1 og 10') 
-  : numbers[chosenBar-1] = parseInt(inputValue);
-  show();
-}
-
-function fjerneStolpe(){
-  numbers.splice((chosenBar-1), 1);
-  chosenBar = undefined;
-  velgStolpe();
-  show();
-}
-
-function leggTilStolpe(){
-  inputValue > 10 || inputValue == undefined || inputValue < 1 
-  ? alert('Du må velge en verdi mellom 1 og 10. Det blir høyden på stolpen.') 
-  : numbers.push(parseInt(inputValue));
-  inputValue = undefined;
-  show();
-}
